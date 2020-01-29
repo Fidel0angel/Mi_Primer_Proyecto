de79e95e0ca3fc5cdc780184d03607301896c249
Fidel0angel <60446263+Fidel0angel@users.noreply.github.com>
2020-01-29 13:58:20 -0600

    Add files via upload

---
+++ Mi_Primer_Proyecto/Mi_Primer_Proyecto/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Mi_Primer_Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.lblnum2 = new System.Windows.Forms.Label();
+            this.lblrespuesta = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.btnprocesar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Location = new System.Drawing.Point(25, 43);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(35, 13);
+            this.lblnum1.TabIndex = 0;
+            this.lblnum1.Text = "Num1";
+            // 
+            // lblnum2
+            // 
+            this.lblnum2.AutoSize = true;
+            this.lblnum2.Location = new System.Drawing.Point(141, 43);
+            this.lblnum2.Name = "lblnum2";
+            this.lblnum2.Size = new System.Drawing.Size(33, 13);
+            this.lblnum2.TabIndex = 1;
+            this.lblnum2.Text = "num2";
+            this.lblnum2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblrespuesta
+            // 
+            this.lblrespuesta.AutoSize = true;
+            this.lblrespuesta.Location = new System.Drawing.Point(98, 83);
+            this.lblrespuesta.Name = "lblrespuesta";
+            this.lblrespuesta.Size = new System.Drawing.Size(73, 13);
+            this.lblrespuesta.TabIndex = 2;
+            this.lblrespuesta.Text = "Respuesta:  ?";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(66, 36);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(45, 20);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(182, 36);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(45, 20);
+            this.txtnum2.TabIndex = 4;
+            // 
+            // btnprocesar
+            // 
+            this.btnprocesar.Location = new System.Drawing.Point(101, 110);
+            this.btnprocesar.Name = "btnprocesar";
+            this.btnprocesar.Size = new System.Drawing.Size(75, 36);
+            this.btnprocesar.TabIndex = 5;
+            this.btnprocesar.Text = "Procesar";
+            this.btnprocesar.UseVisualStyleBackColor = true;
+            this.btnprocesar.Click += new System.EventHandler(this.btnprocesar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnprocesar);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblrespuesta);
+            this.Controls.Add(this.lblnum2);
+            this.Controls.Add(this.lblnum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnum1;
+        private System.Windows.Forms.Label lblnum2;
+        private System.Windows.Forms.Label lblrespuesta;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.Button btnprocesar;
+    }
+}
+
---
+++ Mi_Primer_Proyecto/Mi_Primer_Proyecto/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mi_Primer_Proyecto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnprocesar_Click(object sender, EventArgs e)
+        {
+            sbyte num1 = sbyte.Parse(txtNum1.Text);
+            sbyte num2 = sbyte.Parse(txtnum2.Text);
+            lblrespuesta.Text = "Respuesta" + (num1 + num2);
+        }
+    }
+}
